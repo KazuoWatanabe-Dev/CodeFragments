d88f1d08d62554a3d9b02e3e04c61598e76774e3
kazuowatanabe.dev <kazuowatanabe.dev@gmail.com>
2025-04-30 17:38:09 -0300

    Update isUserInAssignmentGroup.js

--- Scripted-Conditions/isUserInAssignmentGroup.js
+++ Scripted-Conditions/isUserInAssignmentGroup.js
@@ -1,4 +1,7 @@
 //Scripted-Condition: Checks if the user belongs to the assigned group.
+//Tools for ServiceNow required </>
+
+//Version 1.0
 
 var userGroups = gs.getUser().getMyGroups();
     var assignGroup = current.assignment_group;
@@ -6,4 +9,12 @@ var userGroups = gs.getUser().getMyGroups();
         return userGroups.indexOf(assignGroup.sys_id.toString()) !== -1;
     }
     return false;
+})();
+
+//Version 2.0
+
+(function() {
+    if (current.approval == 'approved' || current.approval == 'rejected') return false;
+    var groupId = current.assignment_group?.sys_id.toString();
+    return groupId && gs.getUser().getMyGroups().includes(groupId);
 })();
